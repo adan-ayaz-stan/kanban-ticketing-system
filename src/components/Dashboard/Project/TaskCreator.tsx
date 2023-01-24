c8bd987db07c6d397b8feaf5cfab67f4da67ee28
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-01-24 20:53:29 +0500

    Almost done on task creation

--- src/components/Dashboard/Project/TaskCreator.tsx
+++ src/components/Dashboard/Project/TaskCreator.tsx
@@ -1,6 +1,11 @@
+import { useFormik } from "formik";
+import { useEffect, useRef } from "react";
 import Select from "react-select";
 
 export default function TaskCreator({ projectData }) {
+  const dateBoxRef = useRef(null);
+  const priortyRef = useRef(null);
+
   const assignOptions = projectData.members.map((ele: String) => {
     return { value: ele, label: ele };
   });
@@ -10,32 +15,103 @@ export default function TaskCreator({ projectData }) {
     { value: "high", label: "High" },
   ];
 
-  return (
-    <div className="p-2 m-3 border-2 text-black bg-white">
-      <h1 className="font-bold">Create new task</h1>
+  // SETTING TODAYS DATE AS DEFAULT
+  useEffect(() => {
+    dateBoxRef.current.valueAsDate = new Date();
+  }, []);
 
-      <form className="flex flex-col gap-2">
+  // FORM HANDLING
+  const formik = useFormik({
+    initialValues: {
+      task_name: "",
+      task_description: "",
+      assign_to: "",
+      priorty: "",
+      due_date: "",
+    },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div className="p-2 m-3 border-[1px] text-white bg-blue-800 rounded-lg">
+      <h1 className="w-fit mx-auto my-2 font-bold text-xl uppercase">
+        Create new task
+      </h1>
+
+      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-2">
         <input
           type={"text"}
           name="task_name"
           placeholder="Task Title"
-          className="w-full p-2 border-2 rounded-lg"
+          onChange={formik.handleChange}
+          value={formik.values.task_name}
+          className="w-full text-black p-2 border-2 rounded-lg focus:outline-none"
+          required
         />
 
         <textarea
           rows={3}
           name="task_description"
           placeholder="Task Description"
-          className="p-2 border-2 resize-y rounded-lg"
+          onChange={formik.handleChange}
+          value={formik.values.task_description}
+          className="p-2 text-black border-2 resize-y rounded-lg focus:outline-none"
+          required
         />
 
-        <div className="flex flex-row justify-between gap-3">
-          <Select options={assignOptions} className="w-full" />
+        <div className="flex flex-col md:flex-row justify-between gap-3">
+          <div className="w-full flex flex-col">
+            <label>Assign To: </label>
+            <Select
+              name="assign_to"
+              options={assignOptions}
+              className="text-black rounded-lg"
+              defaultValue={assignOptions[0]}
+              onChange={(value) => {
+                console.log(value);
+              }}
+              required
+            />
+          </div>
 
-          <Select options={priortyOptions} className="w-full" />
+          <div className="w-full flex flex-col">
+            <label>Task Priorty: </label>
+            <Select
+              name="priorty"
+              options={priortyOptions}
+              className="text-black rounded-lg"
+              defaultValue={priortyOptions[0]}
+              onChange={(value) => {
+                console.log(value);
+              }}
+              ref={priortyRef}
+              required
+            />
+          </div>
 
-          <input type={"date"} name="due_date" className="w-full" />
+          <div className="w-full flex flex-col">
+            <label>Task Due Date: </label>
+            <input
+              type={"date"}
+              name="due_date"
+              className="px-2 py-[.4em] text-black text-center border-[1px] border-gray-300 rounded focus:outline-none"
+              ref={dateBoxRef}
+              onChange={formik.handleChange}
+              value={formik.values.due_date}
+              required
+            />
+          </div>
         </div>
+
+        <button
+          type="submit"
+          className="w-fit px-2 py-1 mx-auto font-bold uppercase text-blue-600 bg-white rounded"
+        >
+          Create Task
+        </button>
       </form>
     </div>
   );
