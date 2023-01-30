d49723e2dc6c837ffdfded32dadc9a3c1eb69726
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-01-30 13:24:49 +0500

    Disable type checking for a sample deployment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
